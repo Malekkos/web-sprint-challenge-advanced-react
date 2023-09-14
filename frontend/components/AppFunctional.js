1abc1a68662b054f45b0ac7e62cb5b7e5e159060
Christian Anderson <tehninjawaffle@gmail.com>
2023-09-13 22:23:24 -0700

    Did some thinking to see what my logic might be, added state that was suggested plus one for my coords, which i think i might need for how i intend to change coord location

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,15 +1,46 @@
-import React from 'react'
-
+import  React from 'react'
+import { useState } from "react"
 // Suggested initial states
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
+
+// THOUGHTS
+
+// when moving up, subtract by 3
+// When moving down, add by 3
+// When moving right, add 1
+// when moving left, subtract 1
+// we know where the block is due to index measuring from 0 to 8 (9 spaces)
+
+// finding coord's / row and column
+// from 0 to 8, just have all the cords aka 1,1 is at index 0 1,2 is index 1
+// or, moving left subtracts the first num by 1
+// movign right increases the first numb by 1
+// moving down increases the second num by 1
+// moving up decreases the second num by 1
+
+// Error message
+//The above can also be used for the error message i think
+// When the action of moving would place the number at 0 or 4, return the error message instead with whatever the move was
+
+
+// You moved x times
+// very easy.
+const initialCoords = [2,2]
+
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  let [index, setIndex] = useState(initialIndex)
+  let [coordinates, setCoordinates] = useState(initialCoords)
+  let [steps, setSteps] = useState(initialSteps)
+  let [message, setMessage] = useState(initialMessage)
+  let [email, setEmail] = useState(initialEmail)
 
+  console.log(coordinates)
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
