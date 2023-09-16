0ff8a35149dcde3d2ad2f47d7023c621b7fceb46
Christian Anderson <tehninjawaffle@gmail.com>
2023-09-15 23:03:53 -0700

    changed most of appClass to reflect appFunctional, would have done all of it but there is a weird error but kinda not? The message SHOULDNT work on functional, its just console logging. But it does? Changed to function like it should on AppClass and they.. work the same? Will look into it

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -16,31 +16,88 @@ const initialState = {
 export default class AppClass extends React.Component {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  constructor() {
+    super()
+    this.state = initialState
+  }
+
+
 
   getXY = () => {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+    let x = this.state.index % 3 + 1;
+    let y = this.state.index / 3 + 1;
+    let roundedY = Math.floor(y)
+    console.log(x, roundedY)
+    return {x , roundedY}
   }
-
-  getXYMessage = () => {
+  
+  getXYMessage = (direction) => {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    if(this.getXY().x === 1 && direction == "left") {
+      this.setState({message: `you can't go ${direction}`})
+    }
+    if(this.getXY().roundedY === 1 && direction === "up") {
+      this.setState({message: `you can't go ${direction}`})
+    }
+    if(this.getXY().x === 3 && direction === "right") {
+      this.setState({message: `you can't go ${direction}`})
+    }
+    if(this.getXY().roundedY === 3 && direction ==="down") {
+      this.setState({message: `you can't go ${direction}`})
+    }
   }
 
   reset = () => {
     // Use this helper to reset all states to their initial values.
+    this.setState(initialState)
   }
 
   getNextIndex = (direction) => {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    // console.log(this.getXY().x, this.getXY().roundedY)
+    if (direction === "left" && this.getXY().x !== 1) {
+      this.setState((prevState) => {
+        return {index: prevState.index - 1, steps: prevState.steps + 1}
+      })
+      // setSteps(steps + 1)
+    } else {
+      this.getXYMessage(direction)
+    }
+    if (direction === "down" && this.getXY().roundedY !== 3) {
+      this.setState((prevState) => {
+        return {index: prevState.index + 3, steps: prevState.steps + 1}
+      })
+    } else {
+      this.getXYMessage(direction)
+    }
+    if (direction === "right" && this.getXY().x !== 3) {
+      this.setState((prevState) => {
+        return {index: prevState.index + 1, steps: prevState.steps + 1}
+      })
+    } else {
+      this.getXYMessage(direction)
+    }
+    if (direction === "up" && this.getXY().roundedY !== 1) {
+      this.setState((prevState) => {
+        return {index: prevState.index - 3, steps: prevState.steps + 1}
+      })
+    } else {
+      this.getXYMessage(direction)
+    }
   }
 
   move = (evt) => {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    let direction = evt
+
+    this.getNextIndex(direction)
   }
 
   onChange = (evt) => {
@@ -52,34 +109,36 @@ export default class AppClass extends React.Component {
   }
 
   render() {
+    this.getXY()
+    console.log(this.state)
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.getXY().x}, {this.getXY().roundedY})</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === this.state.index ? ' active' : ''}`}>
+                {idx === this.state.index ? 'B' : null}
               </div>
             ))
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={() => {this.move("left")}} id="left">LEFT</button>
+          <button onClick={() => {this.move("up")}} id="up">UP</button>
+          <button onClick={() => {this.move("right")}} id="right">RIGHT</button>
+          <button onClick={() => {this.move("down")}} id="down">DOWN</button>
+          <button onClick={() => {this.reset()}} id="reset">reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} id="email" type="email" placeholder="type email"></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
