9a4329b39cfd3ee5f0d26288cad42f7499d020e6
Christian Anderson <tehninjawaffle@gmail.com>
2023-09-15 22:08:42 -0700

    After a painstaking 2 days of work, being frustrated out of my gourd, I FINALLY figured out how to make the tiles move without them overflowing the sides. Nothing in my natural born history will triumph this victory

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -44,11 +44,10 @@ export default function AppFunctional(props) {
     // It's enough to know what index the "B" is at, to be able to calculate them.
     let x = index % 3 + 1;
     let y = index / 3 + 1;
-    Math.floor(y)
-    console.log(x, y)
-    return {x,y}
+    let roundedY = Math.floor(y)
+    // console.log(x, roundedY)
+    return {x , roundedY}
   }
-  getXY()
 // Determining how to get coords
 // using the index of b
 //
@@ -60,15 +59,27 @@ export default function AppFunctional(props) {
 
 
 
-  function getXYMessage() {
+  function getXYMessage(direction) {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
 
-    if(getXY(x) === 1 && direction === "down" || "up" || "left") {console.log(setMessage(`you can't go ${direction}`))}
-    if(getXY(y) === 1 && direction === "up" || "left" || "right") {console.log(setMessage(`you can't go ${direction}`))}
-    if(getXY(x) === 3 && direction === "up" || "right" || "down") {console.log(setMessage(`you can't go ${direction}`))}
-    if(getXY(y) === 3 && direction === "right" || "down" || "left") {console.log(setMessage(`you can't go ${direction}`))}
+    if(getXY().x === 1 && direction == "left") {
+      console.log(setMessage(`you can't go ${direction}`))
+      return index
+    }
+    if(getXY().roundedY === 1 && direction === "up") {
+      console.log(setMessage(`you can't go ${direction}`))
+      return index
+    }
+    if(getXY().x === 3 && direction === "right") {
+      console.log(setMessage(`you can't go ${direction}`))
+      return
+    }
+    if(getXY().roundedY === 3 && direction ==="down") {
+      console.log(setMessage(`you can't go ${direction}`))
+      return
+    }
   }
 
   function reset() {
@@ -83,11 +94,29 @@ export default function AppFunctional(props) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
-
-    if (direction === "up") {setIndex(index - 3)}
-    if (direction === "down") {setIndex(index + 3)}
-    if (direction === "right") {setIndex(index + 1)}
-    if (direction === "left") {setIndex(index - 1)}
+    // getXYMessage(direction)
+    console.log(getXY().x, getXY().roundedY)
+    if (direction === "left" && getXY().x !== 1) {
+      setIndex(index - 1)
+      
+    } else {
+      getXYMessage(direction)
+    }
+    if (direction === "down" && getXY().roundedY !== 3) {
+      setIndex(index + 3)
+    } else {
+      getXYMessage(direction)
+    }
+    if (direction === "right" && getXY().x !== 3) {
+      setIndex(index + 1)
+    } else {
+      getXYMessage(direction)
+    }
+    if (direction === "up" && getXY().roundedY !== 1) {
+      setIndex(index - 3)
+    } else {
+      getXYMessage(direction)
+    }
 
   }
   
@@ -95,12 +124,7 @@ export default function AppFunctional(props) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
     let direction = evt
-    console.log(coordinates.x)
-    console.log(coordinates.y)
-    if(coordinates[0] === 1 && direction === "down" || "up" || "left") {console.log("pello")}
-    if(coordinates[1] === 1 && direction === "up" || "left" || "right") {console.log("hello")}
-    if(coordinates[0] === 3 && direction === "up" || "right" || "down") {console.log("gello")}
-    if(coordinates[1] === 3 && direction === "right" || "down" || "left") {console.log("fello")}
+
     getNextIndex(direction)
     }
 
@@ -116,14 +140,14 @@ export default function AppFunctional(props) {
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates {getXY().x}, {getXY().y}</h3>
+        <h3 id="coordinates">Coordinates {getXY().x}, {getXY().roundedY}</h3>
         <h3 id="steps">You moved {steps} times</h3>
       </div>
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+            <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
+              {idx === index ? 'B' : null}
             </div>
           ))
         }
