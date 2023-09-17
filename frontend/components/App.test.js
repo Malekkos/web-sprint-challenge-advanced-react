3fcc8c448d9f45c5ae1ade7f2d37e3081e574798
Christian Anderson <tehninjawaffle@gmail.com>
2023-09-17 13:14:04 -0700

    Installed user event. Could not get fireEvent to work. Finished my 2nd test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,13 +1,15 @@
 // Write your tests here
 import AppClass from "./AppClass"
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import React from "react"
 import "@testing-library/jest-dom"
+import userEvent from '@testing-library/user-event'
+
 //Test that visible text from headings buttons links are on the screen
 //test that typing on the input results in its value changing to the entered text
 // PERSONAL \/
 // check if attempts to break the program work
-
+// const {default: userEvent} = require('@testing-library/user-event')
 test('sanity', () => {
   expect(true).toBe(true)
 })
@@ -18,7 +20,7 @@ test("Harcoded text is visible on the screen", () => {
   let coordinatesText = screen.getByText("Coordinates (2, 2)")
   // console.log(coordinatesText)
   let leftButtonText = screen.getByRole("button", { name: "LEFT"})
-  console.log(leftButtonText)
+  // console.log(leftButtonText)
   let upButtonText = screen.getByText("UP")
   let rightButtonText = screen.getByText("RIGHT")
   let downButtonText = screen.getByText("DOWN")
@@ -26,6 +28,20 @@ test("Harcoded text is visible on the screen", () => {
   // let submitButtonText = screen.getByRole("input#submit")
 
   //assert
-  expect(coordinatesText).toBeTruthy() //pr sure this means it exists
-  expect(leftButtonText).toHaveTextContent("LEFT")
+  expect(coordinatesText).toHaveTextContent("Coordinates (2, 2)")
+  expect(leftButtonText).toHaveTextContent(/LEFT/i)
+  expect(upButtonText).toHaveTextContent(/UP/i)
+  expect(rightButtonText).toHaveTextContent(/RIGHT/i)
+  expect(downButtonText).toHaveTextContent(/DOWN/i)
+  expect(resetButtonText).toHaveTextContent(/reset/i)
+})
+
+test("Typing into a input, email, changes its value", () => {
+  render (<AppClass />)
+
+  let emailInput = screen.getByRole("textbox")
+  userEvent.click(emailInput)
+  userEvent.type(emailInput, "garcia@mojave.com")
+
+ waitFor(() => {expect(emailInput).toHaveTextContent(/garcia@mojave.com/i)})
 })
